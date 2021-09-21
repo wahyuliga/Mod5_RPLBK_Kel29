7b1dd3ead50cfc6578c5ff15417049a37115d633
wahyuliga <wahyuligas@yahoo.com>
2021-09-21 17:52:09 +0700

    Adding modal

--- tugas-modul5-29/src/Laptop.js
+++ tugas-modul5-29/src/Laptop.js
@@ -3,22 +3,33 @@ import axios from "axios";
 import AppBar from "@mui/material/AppBar";
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const DeskripsiContext = createContext();
+
 export default function Laptop()  {
     const [laptop, setLaptop] = useState([]);
-
-    // useEffect((deskripsi) => {
-    //     alert("Deskripsi : " + deskripsi);
-    // }, [])
-
+    const [deskripsi, setDeskripsi] = useState([laptop.deskripsi]);
+    const [open, setOpen] = useState(false);
+    const desc = useContext(DeskripsiContext);
+    const handleClose = () => setOpen(false);
     useEffect(() => {
         axios({
             method: "get",
@@ -54,7 +65,7 @@ export default function Laptop()  {
                                     image={results.image}
                                     alt={results.name}
                                 />
-                                <CardActionArea>
+                                <CardActionArea onClick={() => {setOpen(true)}}>
                                     <CardContent style={{ backgroundColor: '#dff8ef' }}>
                                         <Typography>Name: {results.name}</Typography>
                                         <Typography>Harga: {results.harga}​​​​​​</Typography>
@@ -66,6 +77,36 @@ export default function Laptop()  {
                     );
                 })}
             </Grid>
+            <DeskripsiContext.Provider value={deskripsi}>
+                <div>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                Deskripsi Laptop
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                Coba dulu modalnya
+                                {/* {desc.map((spek) => {
+                                    return (
+                                        <div>
+                                            <p>{spek.Layar}</p>
+                                            <p>{spek.Prosesor}</p>
+                                            <p>{spek.Grafis}</p>
+                                            <p>{spek.RAM}</p>
+                                            <p>{spek.Penyimpanan}</p>
+                                        </div>          
+                                    );
+                                })} */}
+                            </Typography>
+                        </Box>
+                    </Modal>
+                </div>
+            </DeskripsiContext.Provider>
         </div>
     );
 }
